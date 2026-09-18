d5b19c872b10ba489c3b0a0b8abaf6ceecdc1f7f
Prince Kumar Ray <princekumar926425@gmail.com>
2026-09-18 14:46:11 +0530

    Time: 0 ms (100%), Space: 9.2 MB (77.97%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int ans=0;
+        for(char ch:s){
+            ans^=ch;
+        }
+        for(char ch:t){
+            ans^=ch;
+        }
+        return ans;
+    }
+};
